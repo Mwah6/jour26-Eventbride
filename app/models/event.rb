1fe242dc8527f1f7932be85dfc7f2a2b66a0c10b
Mwah6 <sebastien.broin@gmail.com>
2019-05-06 18:18:17 +0200

    1er commit

---
+++ app/models/event.rb
@@ -0,0 +1,28 @@
+class Event < ApplicationRecord
+  validate :start_date_cannot_be_in_the_past,
+  :duration_multiple_of_five
+  validates :start_date, presence: true
+  validates :duration, presence: true
+  validates :title, presence: true, length: { in: 5..140 }
+  validates :description, presence: true, length: { in: 20..1000 }
+  validates :price, presence: true, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 1000}
+  validates :location, presence: true
+
+  belongs_to :administrator, class_name: "User"
+
+  has_many :attendances
+  has_many :users, through: :attendances
+  
+  def start_date_cannot_be_in_the_past
+    if start_date.present? && start_date < Date.now
+      errors.add(:start_date, "can't be in the past")
+    end
+  end
+
+  def duration_multiple_of_five
+    if duration.present? && (duration % 5 != 0 || duration < 0)
+      errors.add(:duration, "doit être un multiple de 5 strictement positif")
+    end
+  end
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  after_create :welcome_send
+
+  has_many :attendances
+  has_many :administrated_events, foreign_key: 'administrator_id', class_name: "Event"
+  has_many :events, through: :attendances
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+end
