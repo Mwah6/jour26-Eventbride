fc71797425d06fbcf4607811c627c30443618d7a
Mwah6 <sebastien.broin@gmail.com>
2019-05-08 03:48:55 +0200

    dernier dernier dernier commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/index'
-  get 'static_pages/secret'
+
   devise_for :users do
   get "/users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session
 end
   root 'events#index'
-  # root 'static_pages#index'
-
-
 
   resources :events, only: [:index, :new, :create, :show]
   resources :users, only: [:show]
